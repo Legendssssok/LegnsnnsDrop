9fbdd38bc26752ac30e50af6201d208b4cc318c6
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2023-09-09 12:40:18 +0530

    Update __main__.py

--- drop/__main__.py
+++ drop/__main__.py
@@ -1,1 +1,10 @@
+import asyncio
 
+from .core.clients import *
+
+loop = asyncio.get_event_loop()
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(Start_DropBot())
+    print(" Good Bye ! ")
