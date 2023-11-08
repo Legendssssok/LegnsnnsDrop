69cc9b734cb31addafa151e0bd8c1defece99f40
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2023-11-08 18:46:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+import os
+
 API_ID = 11573285
 API_HASH = "f2cc3fdc32197c8fbaae9d0bf69d2033" 
 BOT_TOKEN = "6003171489:AAH3OGi0UVlNWvFOmFBy_OoUjAoh-sayEt4"
 INVITE_LINK = "https:....."
+REDIS_URI = None
+REDIS_HOST = None
+MONGO_URI = None
+DATABASE_URL = os.environ.get("DATABASE_URL", None)
