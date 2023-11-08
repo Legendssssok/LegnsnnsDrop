228c5c47fa815b9176d74e35eeca5097ece80220
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2023-11-08 18:41:07 +0530

    Update start.py

--- drop/plugins/start.py
+++ drop/plugins/start.py
@@ -1,13 +1,20 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
 from drop.Config import INVITE_LINK
+from drop.database.users_db import *
 # Start Message
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot, msg):
     user = await bot.get_me()
     mention = user.mention
     await bot.send_message(msg.chat.id, "Thank you for joining, you've been subscribed to our bot. When we do a drop, you will receive a notification. ❤️")
+    add_users(msg.chat.id)
 
+
+@Client.on_message(filters.private & filters.incoming & filters.command("stats"))
+async def stats(bot, msg):
+    id = get_users()
+    await bot.send_message(msg.chat.id, f"Total Users = {id}")
 
 @Client.on_message(filters.private & filters.incoming & filters.command("invite"))
 async def invite(bot, msg):
