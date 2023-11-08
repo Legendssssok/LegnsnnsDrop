29d206f82fc6b5b5625dab03f34c809a0e65d5d3
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2023-11-08 19:02:30 +0530

    Create storefile_db.py

---
+++ drop/database/storefile_db.py
@@ -0,0 +1,25 @@
+from drop.database.all_db import legend_db
+
+
+def get_store():
+    return legend_db.get_key("STORE") or []
+
+
+def is_store(file):
+    return id in get_store()
+
+
+def add_store(file):
+    tusers = get_store()
+    if not file in tusers:
+        tusers.append(file)
+        return legend_db.set_key("STORE", tusers)
+    return False
+
+
+def del_store(file):
+    if is_store(file):
+        tusers = get_store()
+        tusers.remove(file)
+        return legend_db.set_key("STORE", tusers)
+    return False
