2c79681b969bce6a568cf15a5679e82bd49be102
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2023-11-09 06:08:22 +0530

    Update and rename time.py to time_db.py

--- drop/database/time_db.py
+++ drop/database/time_db.py
@@ -5,7 +5,7 @@ def get_time():
     return legend_db.get_key("TIME") or {}
 
 
-def add_store(id, time):
+def add_time(id, time):
     tusers = get_time()
     tusers[id] = time
     return legend_db.set_key("TINE", tusers)
