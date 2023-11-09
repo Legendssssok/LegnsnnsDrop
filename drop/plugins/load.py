c6b44d07d006a52355962149adfe4722279f6775
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2023-11-09 06:08:16 +0530

    Update load.py

--- drop/plugins/load.py
+++ drop/plugins/load.py
@@ -34,7 +34,7 @@ async def claim(bot, msg):
     time_since_last_message = time.time() - last_message_times[user_id]
     if time_since_last_message < int(max_time):
         remaining_time = int(max_time) - time_since_last_message
-        cooldown_message = f"Please wait {int(remaining_time / 60)} minutes & {int(remaining_time % 60)} seconds before posting another message to the channel.\n\n**Your post is added to queue & will be posted after {int(remaining_time / 60)} minutes & {int(remaining_time % 60)} seconds automatically.**"
+        cooldown_message = f"Please wait {int(remaining_time / 3600)} hours & {int(remaining_time / 60)} minutes & {int(remaining_time % 60)} seconds before posting another message to the channel.\n\n**Your post is added to queue & will be posted after {int(remaining_time / 60)} minutes & {int(remaining_time % 60)} seconds automatically.**"
         return await message.reply_text(cooldown_message)
     owo = get_store()
     file = owo[0]
