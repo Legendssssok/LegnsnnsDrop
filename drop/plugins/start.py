493b8ef635eebfc8d412c31360fdf8281d6d5a45
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2023-11-09 06:43:39 +0530

    Update start.py

--- drop/plugins/start.py
+++ drop/plugins/start.py
@@ -19,6 +19,32 @@ async def stats(bot, msg):
     file = get_store()
     await bot.send_message(msg.chat.id, f"Total Users = {len(id)}\n\nTotal File Stored = {len(file)}")
 
+@Client.on_message(filters.private & filters.incoming & filters.command("send"))
+async def send(bot, msg):
+    id = get_users()
+    lol = await msg.reply_text("Send me Broadcast Message\n\nTo Cancel : Click /cancel")
+    br_msg = await bot.listen(lol)
+    success = 0
+    fail = 0
+    if br_msg.text = "/cancel":
+       return await msg.reply_text("Cancelled Successfully")
+    start = datetime.now()
+    for i in id:
+        try:
+            await bot.send_message(int(i), br_msg)
+            success += 1
+        except BaseException:
+            fail += 1
+    end = datetime.now()
+    time_taken = (end - start).seconds
+    await bot.send_message(msg.chat.id, 
+                           f"""
+**Broadcast completed in {time_taken} seconds.**
+Total Users in Bot - {len(id)}
+**Sent to** : `{success} users.`
+**Failed for** : `{fail} user(s).`""",
+                          )
+
 
 @Client.on_message(filters.private & filters.incoming & filters.command("invite"))
 async def invite(bot, msg):
