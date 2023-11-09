8e09a127d38585e78cdf9fc44de0297659e3128c
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2023-11-09 06:49:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ BOT_TOKEN = "6003171489:AAH3OGi0UVlNWvFOmFBy_OoUjAoh-sayEt4"
 INVITE_LINK = "https:....."
 REDIS_URI = None
 REDISHOST = None
-MONGO_URI = None
+MONGO_URI = os.environ.get("MONGO_URI", None)
 DATABASE_URL = os.environ.get("DATABASE_URL", None)
 MAX_TIME = 86400
