2ff005ebe03f8ba684557e90168258c58dbba0e6
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-12-08 21:05:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 
 API_ID = 11573285
 API_HASH = "f2cc3fdc32197c8fbaae9d0bf69d2033" 
-BOT_TOKEN = "6442067114:AAHqkznhUCKFMdU-nNJOMyKC85Yd9F6Wlvs"
-INVITE_LINK = "https:....."
+BOT_TOKEN = "6838711431:AAEFG20O5_DyB-UXO16g9zRzTv_nqX6Ljg4"
+INVITE_LINK = "https://t.me/+xAgm86wVwS9mMmNk"
+MUST_JOIN = "https://t.me/+xAgm86wVwS9mMmNk"
 REDIS_URI = None
 REDISHOST = None
 MONGO_URI = os.environ.get("MONGO_URI", None)
