f405be1fd937d9c9f18b942e77632c57b7e1bab3
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-12-25 20:46:50 +0530

    Update musjoin.py

--- drop/plugins/musjoin.py
+++ drop/plugins/musjoin.py
@@ -12,8 +12,9 @@ async def must_join_channel(bot: Client, msg: Message):
         try:
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+            try:
+            if str(MUST_JOIN).isalpha():
+                link = "https://t.me/" + MUST_JOIN.
             else:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
