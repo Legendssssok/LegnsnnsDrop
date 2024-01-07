468301f98bc12503c8d65d24789d103472dd1962
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-07 07:02:20 +0530

    Update time_db.py

--- drop/database/time_db.py
+++ drop/database/time_db.py
@@ -4,7 +4,10 @@ from drop.database.all_db import legend_db
 def get_time():
     return legend_db.get_key("TIME") or {}
 
-
+def clean_time():
+    tusers = {}
+    return legend_db.set_key("TIME", tusers)
+    
 def add_time(id, time):
     tusers = get_time()
     tusers[id] = time
