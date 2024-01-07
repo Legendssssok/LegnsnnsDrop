4bc5978095af133105978bee525944d1622ebf87
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-07 07:03:50 +0530

    Update load.py

--- drop/plugins/load.py
+++ drop/plugins/load.py
@@ -14,7 +14,8 @@ max_time = MAX_TIME
 async def unload(bot, msg):
     clean_store()
     await msg.reply_text(f"Successfully Unloaded All")
-    
+
+
 @Client.on_message(filters.private & filters.incoming & filters.command("load"))
 async def load(bot, msg):
     editable = await msg.reply_text("Send me File")
@@ -26,13 +27,34 @@ async def load(bot, msg):
             new_content = content.split("\n")
             for i in new_content:
                 add_store(i)
-        os.remove(x)
         lol = get_store()
         await msg.reply_text(f"Successfully Loaded : {len(lol)}") 
+        os.remove(x)
     except Exception as e:
         await msg.reply_text(f"ERROR : {e}")
         os.remove(x)
+    clean_time()
 
+
+@Client.on_message(filters.private & filters.incoming & filters.command("drop"))
+async def claim(bot, msg):
+    user_id = msg.from_user.id
+    last_message_times = get_time()
+    if user_id in last_message_times:
+        return await msg.reply_text("You've already received this drop item or there is no stock. Stay tuned for the next drops!")
+    owo = get_store()
+    if len(owo) > 0:
+        file = owo[0]
+        await msg.reply_text(f"Successfully generated your drop! Here it is:\n\n • {file}")
+        del_store(file)
+        add_time(msg.from_user.id, time.time())
+    else:
+        return await msg.reply_text(f"You've already received this drop item or there is no stock. Stay tuned for the next drops!")
+    
+    
+
+
+"""
 
 @Client.on_message(filters.private & filters.incoming & filters.command("drop"))
 async def claim(bot, msg):
@@ -59,7 +81,7 @@ async def claim(bot, msg):
     else:
         return await msg.reply_text(f"You've already received this drop item or there is no stock. Stay tuned for the next drops!")
     
-    
+"""
 
 
     
